cb1dc7f6f6f8b758040240cef901560d706b5bd4
JuliaNakano <jumartinsnakano@gmail.com>
2021-09-14 23:05:25 -0300

    solução do exercício Cebolinha

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercicio_Cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frasedigitada, frasecomdislalia;
+            Console.Write("Digite uma flase: ");
+            frasedigitada= Console.ReadLine();
+
+            frasecomdislalia = frasedigitada
+            .Replace("l", "r")
+            .Replace("r", "l")
+            .Replace("R", "L")
+            .Replace("L", "R");
+            
+            Console.WriteLine(frasecomdislalia);
+        }
+    }
+}
